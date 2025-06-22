80a460649e23881ea6493992522855f4917f3d40
Adriandollar <adrianngeumkam0@gmail.com>
2025-06-22 10:10:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const {
   
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('HAIKO~MDX~JEhD0I4K#8ZgE1UVIPTpp05-dRsYov2cHxRZHL1xUCGDnloSyT2c')
 const sessdata = config.SESSION_ID.replace("HAIKO~MDX~", '');
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
